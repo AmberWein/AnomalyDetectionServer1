fdd2248183ec9588e83fbde89cac26a63661bcc4
Nicole Sharabany <nicolesharabani7@gmail.com>
2021-05-25 12:26:20 +0300

    added model files, parsing csv files(TimeSeries), utilities and detectors (currently checked until filling up CF)

---
+++ models/parse_file.js
@@ -0,0 +1,88 @@
+
+// Reading the file using default
+// fs npm package
+const fs = require("fs");
+const Util = require('./anomaly_detection_util.js');
+
+class TimeSeries{
+     headers = null;
+     data = null;
+     constructor(csvPath){
+          this.csvPath = csvPath;
+          let reader = fs.readFileSync(this.csvPath);
+          // Convert the data to String and        // split it in an array
+          let dataLines = reader.toString().split("\r\n");      
+          let result = [];
+
+          // The array[0] contains all the
+          // header columns so we store them
+          // in headers array
+          let headers = dataLines[0].split(',');
+          // create a list for each column
+          for (let i = 0; i < headers.length; i++) {
+               result[i] = [];
+          }
+          
+          // Since headers are separated, we
+          // need to traverse remaining n-1 rows.
+          for (let i = 1; i < dataLines.length - 1; i++) {
+               let currRow = dataLines[i].split(",");
+               for (let j = 0; j < currRow.length; j++){
+                    result[j].push(currRow[j]);
+               }
+          }
+          this.headers = headers;
+          this.data = result;
+     }
+     getNumOfFeatures(){
+          return this.headers.length;
+     }
+     getNumOfRows(){
+          return this.data[0].length;
+     }
+    /* getHeaders(){
+          let reader = fs.readFileSync(this.csvPath);
+          // Convert the data to String and        // split it in an array
+          let dataLines = reader.toString().split("\r\n");
+
+          // All the rows of the CSV will be
+          // converted to JSON objects which
+          // will be added to result in an array
+          
+          // The array[0] contains all the
+          // header columns so we store them
+          // in headers array
+          return dataLines[0].split(',');
+     }
+
+     getData(){          
+          let reader = fs.readFileSync(this.csvPath);
+          // Convert the data to String and        // split it in an array
+          let dataLines = reader.toString().split("\r\n");      
+          let result = [];
+
+          // The array[0] contains all the
+          // header columns so we store them
+          // in headers array
+          let headers = dataLines[0].split(',');
+          // create a list for each column
+          for (let i = 0; i < headers.length; i++) {
+               result[i] = [];
+          }
+          
+          // Since headers are separated, we
+          // need to traverse remaining n-1 rows.
+          for (let i = 1; i < dataLines.length; i++) {
+               let currRow = dataLines[i].split(",");
+               for (let j = 0; j < currRow.length; j++){
+                    result[j].push(currRow[j]);
+               }
+          }
+          this.headers = headers;
+          this.data = result;
+          return result;
+     }*/
+}
+
+module.exports = TimeSeries;
+
