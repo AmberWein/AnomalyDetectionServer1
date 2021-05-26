94d159787b30d0c4ae98d23d7b947c0af6cb19b4
Avia <wolf.avia@gmail.com>
2021-05-26 16:03:12 +0300

    adding json presentation to html- not in right place and needs fixing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express()
+const fs = require('fs')
 app.use(express.urlencoded({
     extended: false
 }))
@@ -17,7 +18,17 @@ const PORT = process.env.PORT || 8080
 
 // home page render
 app.get('/', (req, res) => {
-    res.render('index')
+    //this is for convenience because json tag in html has to be defined. need to fix
+    file = "jsonData.json"
+    fs.exists(file, function(exists) {
+      fs.readFile(file, "utf-8", (error, data) => {
+        if (error) throw error
+        let jsonData = []
+        jsonData.push(data)
+        res.render("index", { jsonData: jsonData })
+      })
+    })
+
 })
 
 var storage = multer.diskStorage({
@@ -42,11 +53,17 @@ app.post('/uploadfile', multipleUploads, (req, res) => {
         let algo = req.body.algo_choice     
         
        let result = model.findAnomalies(req.files['file1'][0].path, req.files['file2'][0].path, algo)
+      
         res.write(result)
        // res.json({ body : 0 })
+
+       
     }
     res.end()
 })
+
+
+
 
 app.listen(PORT, () =>{
     console.log('App is listening...')
