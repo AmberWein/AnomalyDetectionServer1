a9d8b559f218443f475b1da6bef925d20596a09e
Nicole Sharabany <nicolesharabani7@gmail.com>
2021-05-28 14:47:37 +0300

    changed find_anomalies to make detector object to work with given files. currently working from chrome and from python request

--- models/find_anomalies.js
+++ models/find_anomalies.js
@@ -6,11 +6,12 @@ function findAnomalies(train, anomaly, algo) {
     // begin of switch case
     //det = new detector.SimpleAnomalyDetector(train, anomaly)
    // if (algo == 'Simple')   // this is correct
-   if (algo == 'Line')
-        //det = new detector.SimpleAnomalyDetector('C:\\Users\\NicoleS\\Downloads\\Telegram Desktop\\Train_Little.csv', 'C:\\Users\\NicoleS\\Downloads\\Telegram Desktop\\Test_Little.csv')
-        det = new detector.SimpleAnomalyDetector('.\\uploads\\Train_Little.csv', '.\\uploads\\Test_Little.csv')
+   if (algo == 'Simple')
+        det = new detector.SimpleAnomalyDetector(train, anomaly)
+        //det = new detector.SimpleAnomalyDetector('.\\uploads\\Train_Little.csv', '.\\uploads\\Test_Little.csv')
     else
-        det = new detector.HybridAnomalyDetector('.\\uploads\\Train_Little.csv', '.\\uploads\\Test_Little.csv') // this probably wont be the name
+        det = new detector.HybridAnomalyDetector(train, anomaly)
+        //det = new detector.HybridAnomalyDetector('.\\uploads\\Train_Little.csv', '.\\uploads\\Test_Little.csv') // this probably wont be the name
     // end of switch case
     let j = det.anomalies
     /*let j = []
