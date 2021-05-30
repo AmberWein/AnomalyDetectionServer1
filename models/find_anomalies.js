99b0ae9d82113aa0b10a7da4b9562baa5b6e9de1
Avia <wolf.avia@gmail.com>
2021-05-30 21:14:41 +0300

    adding comments

--- models/find_anomalies.js
+++ models/find_anomalies.js
@@ -4,8 +4,10 @@ const fs = require('fs')
 function findAnomalies(train, anomaly, algo) {
     let det 
     // create the anomaly detector according the required algo
+    // simple algorithm chosen
     if (algo == 'Simple')
         det = new detector.SimpleAnomalyDetector(train, anomaly)
+        //hybrid algorithm chosen
     else
         det = new detector.HybridAnomalyDetector(train, anomaly)
 
